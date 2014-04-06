6d4531f3ef675fac1f14b042c7214d2c32d36a7c
Jun Matsushita <jun@iilab.org>
2014-04-06 05:52:11 +0100

    relative path?

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 case "$1" in
-  "") ansible-playbook --syntax-check --list-tasks -i hosts/production playbook/site.yml
-      ansible -m ping -i hosts/production all;;
-  * ) ansible-playbook --syntax-check --list-tasks -i hosts/production playbook/site.yml -l $1
-      ansible -m ping -i hosts/production $1;;
+  "") ansible-playbook --syntax-check --list-tasks -i ./hosts/production ./playbook/site.yml
+      ansible -m ping -i ./hosts/production all;;
+  * ) ansible-playbook --syntax-check --list-tasks -i ./hosts/production ./playbook/site.yml -l $1
+      ansible -m ping -i ./hosts/production $1;;
 esac
