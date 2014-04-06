8a1f8fbf73a508238a4667e0079007eb3e4063ec
Jun Matsushita <jun@iilab.org>
2014-04-06 05:58:39 +0100

    Working on paths

--- test.sh
+++ test.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 case "$1" in
-  "") ansible-playbook --syntax-check --list-tasks -i ./hosts/production ./playbook/site.yml
-      ansible -m ping -i ./hosts/production all;;
-  * ) ansible-playbook --syntax-check --list-tasks -i ./hosts/production ./playbook/site.yml -l $1
-      ansible -m ping -i ./hosts/production $1;;
+  "") ansible-playbook --syntax-check --list-tasks -i $SCRIPTPATH/hosts/production $SCRIPTPATH/playbook/site.yml
+      ansible -m ping -i $SCRIPTPATH/hosts/production all;;
+  * ) ansible-playbook --syntax-check --list-tasks -i $SCRIPTPATH/hosts/production $SCRIPTPATH/playbook/site.yml -l $1
+      ansible -m ping -i $SCRIPTPATH/hosts/production $1;;
 esac
