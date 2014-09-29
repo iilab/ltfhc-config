3e40c13d1c45cbde007d5640e20f47242c5766cb
Jun Matsushita <jun@iilab.org>
2014-09-29 18:42:03 +0200

    Adding modem init strings. These should be harmonised with the mgetting init configuration

---
+++ playbook/files/scripts/sync_status.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# trap handler: print location of last error and process it further
+#
+function the_trap_handler()
+{
+        MYSELF="$0"               # equals to my script name
+        LASTLINE="$1"            # argument 1: last line of error occurence
+        LASTERR="$2"             # argument 2: error code of last command
+        echo "${MYSELF}: line ${LASTLINE}: exit status of last command: ${LASTERR}"
+
+        exit 1
+}
+
+# trap commands with non-zero exit code
+#
+trap 'the_trap_handler ${LINENO} $?' ERR
+
+SD=${BASH_SOURCE%/*}
+uptime
+uustat -a
+uustat -m
+
+# to prevent job congestion for sustained error (critical mode) delete job queue.
