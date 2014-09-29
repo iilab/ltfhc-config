cd1381e5fdf257e9ed4b90f5f9c2765eb1c01b6c
Jun Matsushita <jun@iilab.org>
2014-09-29 18:26:58 +0200

    Adding debugging info on run playbook

---
+++ run_debug.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+case "$2" in
+  "") echo "Usage: run.sh deploy|diagnose|report|repair|update|backup provisioning|development|production|tunnel [machine] [output_directory]";;
+  *) 
+    case "$3" in
+  	  "") if [[ $OSTYPE == darwin* ]]; 
+		  then script -q /dev/null ansible-playbook -v -i $SCRIPTPATH/hosts/$2 $SCRIPTPATH/playbook/site.yml -t $1 2>&1 | tee $SCRIPTPATH/$1.log;
+		  else script -c "ansible-playbook -v -i $SCRIPTPATH/hosts/$2 $SCRIPTPATH/playbook/site.yml -t $1" /dev/null 2>&1 | tee $SCRIPTPATH/$1.log;
+		  fi;;
+       *) case "$4" in
+       	"") if [[ $OSTYPE == darwin* ]]; 
+		    then script -q /dev/null ansible-playbook -v -i $SCRIPTPATH/hosts/$2 $SCRIPTPATH/playbook/site.yml -t $1 -l $3 2>&1 | tee $SCRIPTPATH/$1.log;
+		    else script -c "ansible-playbook -v -i $SCRIPTPATH/hosts/$2 $SCRIPTPATH/playbook/site.yml -t $1 -l $3" /dev/null 2>&1 | tee $SCRIPTPATH/$1.log;
+		    fi;;
+       	 *) if [[ $OSTYPE == darwin* ]]; 
+		    then script -q /dev/null ansible-playbook -v -i $SCRIPTPATH/hosts/$2 $SCRIPTPATH/playbook/site.yml -t $1 -l $3 2>&1 | tee $4/$1.log;
+			else script -c "ansible-playbook -v -i $SCRIPTPATH/hosts/$2 $SCRIPTPATH/playbook/site.yml -t $1 -l $3" /dev/null 2>&1 | tee $4/$1.log;
+			fi;;
+        esac
+	esac
+esac
