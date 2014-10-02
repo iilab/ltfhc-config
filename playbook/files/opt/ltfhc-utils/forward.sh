48e29c374e4bdb61f4f52142a98ec0486643a01f
Jun Matsushita <jun@iilab.org>
2014-10-02 15:34:14 +0300

    Clean up

--- playbook/files/opt/ltfhc-utils/forward.sh
+++ playbook/files/opt/ltfhc-utils/forward.sh
@@ -2,4 +2,4 @@
 iptables -A FORWARD -o eth1 -i wlan0 -s 192.168.42.0/24 -m conntrack --ctstate NEW -j ACCEPT
 iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE
-iptables -A FORWARD -o eth1 -s 192.168.186.0/16 -m conntrack --ctstate NEW -j ACCEPT
+iptables -A FORWARD -o eth1 -s 192.168.168.0/16 -m conntrack --ctstate NEW -j ACCEPT
