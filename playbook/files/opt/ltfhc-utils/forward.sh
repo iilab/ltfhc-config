4d4286cfe939318a9253a105905a1fdc188d344a
Jun Matsushita <jun@iilab.org>
2014-10-11 12:41:10 +0200

    Forwarding rules for 3g link to get internet

--- playbook/files/opt/ltfhc-utils/forward.sh
+++ playbook/files/opt/ltfhc-utils/forward.sh
@@ -3,3 +3,9 @@ iptables -A FORWARD -o eth1 -i wlan0 -s 192.168.42.0/24 -m conntrack --ctstate N
 iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE
 iptables -A FORWARD -o eth1 -s 192.168.168.0/16 -m conntrack --ctstate NEW -j ACCEPT
+
+#!/bin/bash
+iptables -A FORWARD -o ppp0 -i wlan0 -s 192.168.42.0/24 -m conntrack --ctstate NEW -j ACCEPT
+iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+iptables -t nat -A POSTROUTING -o ppp0 -j MASQUERADE
+iptables -A FORWARD -o ppp0 -s 10.108.2.0/16 -m conntrack --ctstate NEW -j ACCEPT
